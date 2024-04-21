7a593bf0c4e9cd0d61f0c4bb85c0dfcb55640d76
José Lopes <jlopesum004@gmail.com>
2024-04-21 18:55:32 +0100

    Error checking and documentation

--- include/server/pipeline.h
+++ include/server/pipeline.h
@@ -1,6 +1,43 @@
+/*
+ * Copyright 2024 Humberto Gomes, José Lopes, José Matos
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file  server/pipeline.h
+ * @brief Handles the execution of pipelines.
+ */
+
 #ifndef PIPELINE_H
 #define PIPELINE_H
 
+/**
+ * @brief Executes a task which is a pipeline.
+ * @param taks Task with programs to be executed in a pipeline.
+ *
+ * @return 1 on failure (check `errno`), or 0 on success.
+ *
+ * | `errno`  | Cause                                                                              |
+ * | -------- | ---------------------------------------------------------------------------------- |                                       |
+ * | `ENOMEM` | `fork()` failure from insuficient memory or from terminated "init" process in      |
+ * |          |  namespace.                                                                        |
+ * | -------- | ---------------------------------------------------------------------------------- |
+ * | `EMFILE` | System call `pipe()` failure, due to system resources limits (see `man 2 pipe`).   |
+ * | -------- | ---------------------------------------------------------------------------------- |
+ * | other    | See `man 2 execve`.                                                                |
+ * | -------- | ---------------------------------------------------------------------------------- |
+ */
 int pipeline_execute(const task_t *task);
 
 #endif
--- src/server/pipeline.c
+++ src/server/pipeline.c
@@ -1,9 +1,36 @@
+/*
+ * Copyright 2024 Humberto Gomes, José Lopes, José Matos
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file  pipeline.c
+ * @brief Implementation of methods in server/pipeline.h.
+ */
+
+#include <stdint.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
 #include "server/task.h"
 
-#include <unistd.h>
-#include <sys/wait.h>
-#include <stdint.h>
-
+/**
+ * @brief Closes file descriptors used for pipe comunication.
+ *
+ * @param fd_pairs Array with file descriptors to be closed.
+ * @param size     Number of pairs of file descriptors in @p fd_pairs.
+ */
 void __close_pipes_descriptors(int (*fd_pairs)[2], size_t size) {
     for (size_t i = 0; i < size; ++i) {
         close(fd_pairs[i][STDOUT_FILENO]);
@@ -12,9 +39,8 @@ void __close_pipes_descriptors(int (*fd_pairs)[2], size_t size) {
 }
 
 int pipeline_execute(const task_t *task) {
-    size_t nprograms;
+    size_t                  nprograms;
     const program_t *const *programs = task_get_programs(task, &nprograms);
-
 
     if (nprograms < 2)
         return 1; /* Not a pipeline */
@@ -24,7 +50,8 @@ int pipeline_execute(const task_t *task) {
     if (pipe(fd_pairs[0]))
         return 1;
 
-    if (!fork()) {
+    pid_t p = fork();
+    if (!p) {
         dup2(fd_pairs[0][STDOUT_FILENO], STDOUT_FILENO);
 
         __close_pipes_descriptors(fd_pairs, 1);
@@ -34,13 +61,16 @@ int pipeline_execute(const task_t *task) {
 
         /* In case of exec failure */
         _exit(127);
+    } else if (p == -1) {
+        return 1;
     }
 
     for (size_t i = 1; i < nprograms - 1; ++i) {
         if (pipe(fd_pairs[i]))
             return 1;
 
-        if (!fork()) {
+        p = fork();
+        if (!p) {
             dup2(fd_pairs[i - 1][STDIN_FILENO], STDIN_FILENO);
             dup2(fd_pairs[i][STDOUT_FILENO], STDOUT_FILENO);
 
@@ -51,10 +81,13 @@ int pipeline_execute(const task_t *task) {
 
             /* In case of exec failure */
             _exit(127);
+        } else if (p == -1) {
+            return 1;
         }
     }
 
-    if (!fork()) {
+    p = fork();
+    if (!p) {
         dup2(fd_pairs[nprograms - 2][STDIN_FILENO], STDIN_FILENO);
 
         __close_pipes_descriptors(fd_pairs, nprograms - 1);
@@ -64,6 +97,8 @@ int pipeline_execute(const task_t *task) {
 
         /* In case of exec failure */
         _exit(127);
+    } else if (p == -1) {
+        return 1;
     }
 
     __close_pipes_descriptors(fd_pairs, nprograms - 1);
