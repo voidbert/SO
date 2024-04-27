e3662a15c5c5a1d44c4d851fe38b62e0d27ca365
voidbert <humbertogilgomes@protonmail.com>
2024-04-27 16:30:38 +0100

    Cleanup task runner

--- src/server/task_runner.c
+++ src/server/task_runner.c
@@ -21,13 +21,14 @@
 
 #include <errno.h>
 #include <fcntl.h>
-#include <limits.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 #include "protocol.h"
 #include "server/task_runner.h"
+#include "util.h"
 
 /** @brief Waits for all children of the current process. */
 void __task_runner_wait_all_children(void) {
@@ -39,18 +40,28 @@ void __task_runner_wait_all_children(void) {
     }
 }
 
-/*
- * @brief Spawns a program with input and output file descriptors.
+/**
+ * @brief Spawns a program with `stdin`, `stdout` and `stderr` file descriptors.
  *
- * @param program Program to be spawned.
- * @param in      Input file descriptor to be duplicated.
- * @param out     Output file descriptor to be duplicated.
- * @param err     Error file descriptor to be duplicated.
+ * @param program Program to be spawned. Mustn't be `NULL`.
+ * @param in      `stdin` file descriptor to be duplicated.
+ * @param out     `stderr` file descriptor to be duplicated.
+ * @param err     `stderr` file descriptor to be duplicated.
  *
- * @retval 0 Success
- * @retval 1 `fork()` failure.
+ * @retval 0 Success.
+ * @retval 1 Failure (check `errno`).
+ *
+ * | `errno`  | Cause                 |
+ * | -------- | --------------------- |
+ * | `EINVAL` | @p program is `NULL`. |
+ * | other    | See `man 2 fork`.     |
  */
 int __task_runner_spawn(const program_t *program, int in, int out, int err) {
+    if (!program) {
+        errno = EINVAL;
+        return 1;
+    }
+
     const char *const *args = program_get_arguments(program);
     pid_t              p    = fork();
     if (p == 0) {
@@ -74,15 +85,14 @@ int __task_runner_spawn(const program_t *program, int in, int out, int err) {
         execvp(args[0], (char *const *) (uintptr_t) args);
 
         /* This error message will end up in the stderr file */
-        fprintf(stderr, "exec() failed!\n");
-        __task_runner_wait_all_children(); /* May block forever */
+        util_error("%s(): exec(\"%s\") failed!\n", __func__, args[0]);
         _exit(1);
     }
-    return 0;
+    return p < 0;
 }
 
 /**
- * @brief   Maximum number of connection reopenings when the other side of the pipe is closed
+ * @brief   Maximum number of connection openings when the other side of the pipe is closed
  *          prematurely.
  * @details This number must be high, as any communication failure means loss of server scheduling
  *          capacity.
@@ -91,31 +101,26 @@ int __task_runner_spawn(const program_t *program, int in, int out, int err) {
 
 /**
  * @brief   Communicates to the parent server that the task has terminated.
- * @details Errors will be outputted to `stderr`.
+ * @details Errors (even those recovered from) will be outputted to `stderr`.
  *
- * @param slot   Slot in the scheduler where this task was scheduled.
- * @param secret Secret random number needed to authenticate the termination of the task.
- * @param error  Whether an error occurred while running the task.
+ * @param slot  Slot in the scheduler where this task was scheduled.
+ * @param error Whether an error occurred while running the task.
  *
  * @retval 0 Success.
  * @retval 1 Failure (unspecified `errno`).
  */
-int __task_runner_warn_parent(size_t slot, uint64_t secret, int error) {
+int __task_runner_warn_parent(size_t slot, int error) {
     struct timespec time_ended = {0};
     (void) clock_gettime(CLOCK_MONOTONIC, &time_ended);
     protocol_task_done_message_t message = {.type       = PROTOCOL_C2S_TASK_DONE,
                                             .slot       = slot,
-                                            .secret     = secret,
                                             .time_ended = time_ended,
                                             .is_status  = 0,
                                             .error      = error};
 
     ipc_t *ipc = ipc_new(IPC_ENDPOINT_CLIENT);
     if (!ipc) {
-        if (errno == ENOENT)
-            fprintf(stderr, "Task completion communication: server's FIFO not found.\n");
-        else
-            perror("Task completion communication: Failed to open() server's FIFO");
+        util_perror("__task_runner_warn_parent(): opening connection");
         return 1;
     }
 
@@ -123,7 +128,7 @@ int __task_runner_warn_parent(size_t slot, uint64_t secret, int error) {
                        &message,
                        sizeof(protocol_task_done_message_t),
                        TASK_RUNNER_WARN_PARENT_MAX_RETRIES)) {
-        perror("Task completion communication: failed to write() message to server");
+        util_perror("__task_runner_warn_parent(): opening writing");
         ipc_free(ipc);
         return 1;
     }
@@ -132,33 +137,35 @@ int __task_runner_warn_parent(size_t slot, uint64_t secret, int error) {
     return 0;
 }
 
-int task_runner_main(tagged_task_t *task, size_t slot, uint64_t secret, const char *outputdir) {
+int task_runner_main(tagged_task_t *task, size_t slot, const char *directory) {
     uint32_t                task_id = tagged_task_get_id(task);
     size_t                  nprograms;
     const program_t *const *programs = task_get_programs(tagged_task_get_task(task), &nprograms);
     if (!programs) {
         task_procedure_t procedure;
         void            *state;
-        (void) task_get_procedure(tagged_task_get_task(task), &procedure, &state);
-        return procedure(state, slot, secret);
+        task_get_procedure(tagged_task_get_task(task), &procedure, &state);
+        return procedure(state, slot);
     }
 
     if (!nprograms)
         return 1;
 
     char outpath[PATH_MAX];
-    snprintf(outpath, PATH_MAX, "%s/%" PRIu32 ".out", outputdir, task_id);
+    snprintf(outpath, PATH_MAX, "%s/%" PRIu32 ".out", directory, task_id);
     int out = open(outpath, O_CREAT | O_WRONLY | O_TRUNC, 0640);
-    if (out == -1) {
-        perror("Failed to create output file! Redirecting output to stdout");
+    if (out < 0) {
+        util_perror(
+            "task_runner_main(): failed to create output file - redirecting output to stdout");
         out = STDOUT_FILENO;
     }
 
     char errpath[PATH_MAX];
-    snprintf(errpath, PATH_MAX, "%s/%" PRIu32 ".err", outputdir, task_id);
+    snprintf(errpath, PATH_MAX, "%s/%" PRIu32 ".err", directory, task_id);
     int err = open(errpath, O_CREAT | O_WRONLY | O_TRUNC, 0640);
     if (err < 0) {
-        perror("Failed to create output file! Redirecting error messages to stderr");
+        util_perror(
+            "task_runner_main(): failed to create output file - redirecting output to stdout");
         err = STDERR_FILENO;
     }
 
@@ -166,21 +173,28 @@ int task_runner_main(tagged_task_t *task, size_t slot, uint64_t secret, const ch
     for (size_t i = 0; i < nprograms - 1; ++i) {
         int fds[2];
         if (pipe(fds)) {
-            fprintf(stderr, "pipe() failed running task %" PRIu32 "!\n", task_id);
+            char error_msg[LINE_MAX] = {0};
+            (void) strerror_r(errno, error_msg, LINE_MAX);
+            util_error("%s(): pipe() failed: %s\n", __func__, error_msg);
+
             __task_runner_wait_all_children(); /* May block forever */
-            __task_runner_warn_parent(slot, secret, 1);
+            __task_runner_warn_parent(slot, 1);
             _exit(1);
         }
 
         if (__task_runner_spawn(programs[i], in, fds[STDOUT_FILENO], err)) {
-            fprintf(stderr, "fork() failed running task %" PRIu32 "!\n", task_id);
+            char error_msg[LINE_MAX] = {0};
+            (void) strerror_r(errno, error_msg, LINE_MAX);
+            util_error("%s(): fork() failed: %s\n", __func__, error_msg);
+
             __task_runner_wait_all_children(); /* May block forever */
-            __task_runner_warn_parent(slot, secret, 1);
+            __task_runner_warn_parent(slot, 1);
             _exit(1);
         }
+
         if (i != 0) /* Don't close stdin */
-            close(in);
-        close(fds[STDOUT_FILENO]);
+            (void) close(in);
+        (void) close(fds[STDOUT_FILENO]);
         in = fds[STDIN_FILENO];
     }
 
@@ -188,8 +202,7 @@ int task_runner_main(tagged_task_t *task, size_t slot, uint64_t secret, const ch
 
     __task_runner_wait_all_children(); /* May block forever */
 
-    close(err);
-    close(out);
-
-    return __task_runner_warn_parent(slot, secret, 0);
+    (void) close(err);
+    (void) close(out);
+    return __task_runner_warn_parent(slot, 0);
 }
